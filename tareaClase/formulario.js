cb374a53d17492f4b98f8b71134a8e290920773e
Albertgiraldo30 <161533615+Albertgiraldo30@users.noreply.github.com>
2024-03-07 15:58:13 -0500

    Tareas clase  marzo

---
+++ tareaClase/formulario.js
@@ -0,0 +1,6 @@
+  const nombreUsuario= document.getElementById ('ingresar_nombre');
+  const telefonoUsuario = document.getElementById ('telefono');
+  const passwordUsuario = document.getElementById ('password');
+
+  nombreUsuario.innerText=albert
+  
